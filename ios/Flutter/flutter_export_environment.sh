35eb6c320624bc3ea2b10a2bd0ccb47d457add1f
Willie π <PaisWillie@gmail.com>
2019-12-28 18:59:55 -0500

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Users\Willie Pai\Desktop\Willie Pai\Software\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Users\Willie Pai\Desktop\Willie Pai\Wolfy App\Wolfy App Repo\wolfy_app"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Users\Willie Pai\Desktop\Willie Pai\Software\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:wolfy_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(WolfyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
